9668dc919724cd905b55688bbac8c1f013c1b60d
Alexey Bezugolnikov <abezugolnikov@list.ru>
2022-08-14 23:35:31 +0300

    Lesson6

---
+++ Task41/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите количество чисел: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите числа");
+
+int[] array = new int[n];
+for (int i=0; i < n; i++)
+    array[i]=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+int count = 0;
+for (int i=0; i < n; i++)
+{
+    if (array[i] > 0)
+    count++;
+}
+Console.WriteLine("Количество чисел больше нуля: " + count);
